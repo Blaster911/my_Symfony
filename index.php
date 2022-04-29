57cf5ccf225cbbe8a818df127cfd1fc561bbe737
Blaster911 <romain588@live.fr>
2022-04-29 10:51:33 +0200

    add files composer and component

--- index.php
+++ index.php
@@ -1,5 +1,12 @@
 <?php
 
-$nale = $_GET['name'];
+require __DIR__ . '/vendor/autoload.php';
 
-printf('hrllo %s', $name);
+$request = Symfony\Component\HttpFoundation\Request::createFromGlobals();
+
+var_dump($request);
+die();
+
+// $name = isset($_GET['name']) ? $_GET['name'] : "World";
+
+// printf('Hello %s', $name);
