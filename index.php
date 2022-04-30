dfd8d23eb3f3bca9320316055225ab1279114c5c
Blaster911 <romain588@live.fr>
2022-04-30 19:19:16 +0200

    refactored and add front controller

--- index.php
+++ index.php
@@ -1,16 +1,9 @@
 <?php
 
 use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\HttpFoundation\Response;
 
 require __DIR__ . '/vendor/autoload.php';
 
+
 $request = Request::createFromGlobals();
-
-$name = $request->query->get('name', 'World');
-
-$response = new Response();
-$response->headers->set('content-type', 'text/html; charset=utf-8');
-$response->setContent(sprintf('Hello %s', htmlspecialchars($name, ENT_QUOTES)));
-
-$response->send();
+var_dump($request->getPathInfo());
